9f2099cb422b8ea0da51ebd291e605694d066502
sejeoong <tpwjd01231@gmail.com>
2021-05-29 13:44:27 +0900

    movie 완료

--- src/Components/MovieComponent.jsx
+++ src/Components/MovieComponent.jsx
@@ -1,21 +1,54 @@
 import MainLayout from '../Layout/MainLayout';
 import Movie from '../Presenters/Movie';
 
+import axios from 'axios';
 
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 const MovieComponent = () => {
 
-    useEffect(() => {
-        console.log('mount');
-        return () => {
-            console.log('unmount');
-        }
+    const CLIENT_ID = 'Wsi3yC_oCzFGr0CemCW5';
+    const SECRET_KEY = 'PiCtTMNmN0';
+    // state [변수 , 변수를 변경하는 함수] = useState(초기값)
+    var [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('어벤져스'); //검색한 단어 -> 네이버에 요청
+
+    const getMovie = () => {
+        // naver 영화 api 호출
+        axios.get('/v1/search/movie.json',  {
+            params: {
+                query: search,
+            }, 
+            headers: {
+               'X-Naver-Client-Id': CLIENT_ID,
+               'X-Naver-Client-Secret': SECRET_KEY, 
+            }
+        }).then((response) => {
+            // movies = response.data.items;
+            setMovies(response.data.items);
+            console.log(response.data.items);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    //componentDidMount
+    useEffect( ()=> {
+        getMovie();
     }, []);
+
+    const changeSearch = (value) => {
+        console.log('changeSearch', value);
+        setSearch(value);
+    }
+
+    useEffect( () => {
+        getMovie();
+    }, [search]);
 
     return (
         <MainLayout>
-           <Movie></Movie>
+           <Movie movies={movies} changeSearch={changeSearch}></Movie>
         </MainLayout>
     );
 }
--- src/Presenters/Movie.jsx
+++ src/Presenters/Movie.jsx
@@ -1,7 +1,31 @@
-const Movie = () => {
+import style from '../resource/css/movie.module.css';
+
+const Movie = (props) => {
+
+    const changeInput = (event) => {
+        console.log(event.target.value, 'changeInput');
+        props.changeSearch(event.target.value);
+    }
+
     return(
         <div>
-            영화
+            <input type='text' onChange={changeInput} />
+            <div className={style.listsMain}>
+                {
+                    props.movies.map((movie) => {
+                        return (
+                            <div className={style.listItem}>
+                                <div>
+                                    <img src={movie.image}/>
+                                </div>
+                                <div>
+                                    <span>{movie.title.replace(/<[^>]+>/g, '')}</span>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
