cb3a93fb27bd3f67757282136acd0b0b2c9f31b6
sejeoong <tpwjd01231@gmail.com>
2021-06-05 13:36:37 +0900

    todo 완료

--- src/Components/TodoComponent.jsx
+++ src/Components/TodoComponent.jsx
@@ -1,10 +1,44 @@
 import MainLayout from '../Layout/MainLayout';
 import Todo from '../Presenters/Todo';
 
+import {useEffect, useState} from 'react';
+
 const TodoComponent = () => {
+    const [lists, setLists] = useState([]);
+
+    const addList = (item) => {
+        setLists(lists.concat(item));
+    }
+
+    //localStorage 저장하기 (JSON 문자열)
+    const saveList = () => {
+        localStorage.setItem('todo', JSON.stringify(lists));
+    }
+
+    //localStorage 불러오기 (JSON화)
+    const getList = () => {
+        setLists(JSON.parse(localStorage.getItem('todo')));
+    }
+
+    const removeListItem = (deleteIndex) => {
+        console.log(deleteIndex);
+        //deleteIndex = string(=>int)
+        //index = int
+        setLists(lists.filter((list,index)=>index!==deleteIndex));
+    }
+
+    useEffect(()=> {
+        getList();
+    }, []);
+
+
+    useEffect(()=>{
+        saveList();
+    },[lists]);
+
     return (
         <MainLayout>
-           <Todo></Todo>
+           <Todo lists={lists} addList={addList} removeListItem={removeListItem}></Todo>
         </MainLayout>
     );
 }
--- src/Presenters/Todo.jsx
+++ src/Presenters/Todo.jsx
@@ -1,7 +1,33 @@
-const Todo = () => {
+const Todo = (props) => {
+
+    const changeInput = (event) => {
+        if(event.key==='Enter') {
+            props.addList(event.target.value);
+            event.target.value = '';
+        }
+    }
+
+    const askRemoveItem = (event) => {
+        // 물어보는거 확인 = true, 취소 = false
+        if (window.confirm('삭제하시겠습니까?')) {
+            //custom data
+            console.log(event.target.dataset.id);
+            props.removeListItem(parseInt(event.target.dataset.id));
+        }   
+    }
+
     return(
         <div>
-            todo
+            <input type='text' onKeyPress={changeInput}/>
+            <div>
+                {
+                    props.lists.map((list, index)=>{
+                        return (
+                            <div onClick={askRemoveItem} data-id={index}>{list}</div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
